8371a192089c32857b0600b6c02d19c70b4fc362
clavinm <clavimoras@gmail.com>
2024-05-27 11:55:29 +0530

    Added: Validation for install button

--- install.js
+++ install.js
@@ -1,31 +1,36 @@
-      let deferredPrompt;
+let deferredPrompt;
 
-      window.addEventListener('beforeinstallprompt', (e) => {
-        e.preventDefault();
-        deferredPrompt = e;
-        const installButton = document.getElementById('installButton');
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  const installButton = document.getElementById('installButton');
 
-        installButton.addEventListener('click', async () => {
-          //  const { outcome } = await deferredPrompt.userChoice;
-          if (deferredPrompt) {
-            deferredPrompt.prompt().then((res) => {
-              if (res.outcome === 'accepted') {
-                deferredPrompt = null;
-                console.log('User accepted the install prompt');
-              } else {
-                console.log('User dismissed the install prompt');
-              }
-            });
-           
-            
-          } else {
-            installButton.style.display = 'none';
-            installButton.textContent = 'Cannot Install';
-            console.log('No install prompt available');
-          }
-        });
-        
-        window.addEventListener('appinstalled', () => {
+  // Check local storage to see if the app is already installed
+
+  installButton.addEventListener('click', async () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt().then((res) => {
+        if (res.outcome === 'accepted') {
+          console.log('User accepted the install prompt');
+          // Save the install state to local storage
+          localStorage.setItem('appInstalled', 'true');
+          deferredPrompt = null;
           installButton.style.display = 'none';
-        });
+        } else {
+          console.log('User dismissed the install prompt');
+        }
       });
+    } else {
+      installButton.style.display = 'none';
+      installButton.textContent = 'Cannot Install';
+      console.log('No install prompt available');
+    }
+  });
+
+  window.addEventListener('appinstalled', () => {
+    console.log('App was installed.');
+    // Save the install state to local storage
+    localStorage.setItem('appInstalled', 'true');
+    installButton.style.display = 'none';
+  });
+});
--- src/features/Home.jsx
+++ src/features/Home.jsx
@@ -1,10 +1,8 @@
-// import React from 'react';
+// import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-// import fashionImage1 from '../assets/card1.png';
-// import Row from '../components/Row';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-// import { useState } from 'react';
 
 const HomeSection = styled.section`
   background-color: #ff7900;
@@ -37,26 +35,6 @@ const ShopNowButton = styled.button`
   text-decoration: none;
 `;
 
-// const CardsContainer = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-// `;
-
-// const Card = styled.div`
-//   width: 100%;
-//   max-width: 50vw;
-//   background-color: #fff;
-//   padding: 2vw;
-//   border-radius: 10px;
-//   box-shadow: 0 2vw 4vw rgba(0, 0, 0, 0.1);
-// `;
-
-// const ProductImage = styled.img`
-//   width: 100%;
-//   border-radius: 2vw;
-// `;
-
 const StyledH1 = styled.h1`
   text-align: center;
   font-weight: bold;
@@ -67,57 +45,40 @@ const StyledH1 = styled.h1`
   margin-bottom: 3vw;
 `;
 
-// const StyledH2 = styled.h2`
-//   text-align: center;
-//   color: black;
-//   font-size: 3vw;
-//   font-style: italic;
-//   margin-top: 5vw;
-// `;
-
-// const BlackBackground = styled.span`
-//   border-radius: 2vw;
-//   color: #ff7600;
-//   background-color: black;
-// `;
-
 export default function Home() {
   const navigate = useNavigate();
+  const [isAppInstalled, setIsAppInstalled] = useState(false);
+
+  useEffect(() => {
+    const appInstalled = localStorage.getItem('appInstalled');
+    if (appInstalled === 'true') {
+      setIsAppInstalled(true);
+    }
+  }, []);
 
   const handleImageCaptureClick = () => {
     navigate('/capture');
   };
+
   return (
     <HomeSection>
       <HomeHeader>
         <StyledH1>Fashion</StyledH1>
         <HeaderRight>
           <div>New Summer Collection</div>
-          <div>Get now and empty your summer vacation</div>
-          <ShopNowButton id="installButton">Install</ShopNowButton>
+          <div>Get now and enjoy your summer vacation</div>
+
+          {!isAppInstalled && (
+            <ShopNowButton id="installButton">Install</ShopNowButton>
+          )}
           <ShopNowButton onClick={handleImageCaptureClick}>
             Image Capture
           </ShopNowButton>
-          <ShopNowButton as={Link} to="/notification">Notification</ShopNowButton> 
+          <ShopNowButton as={Link} to="/notification">
+            Notification
+          </ShopNowButton>
         </HeaderRight>
       </HomeHeader>
-      {/* <CardsContainer>
-        <Row>
-          <Card>
-            <ProductImage src={fashionImage1} alt="Fashion Image 1" />
-          </Card>
-          <Card>
-            <ProductImage src={fashionImage1} alt="Fashion Image 2" />
-          </Card>
-        </Row>
-      </CardsContainer>
-      <div className="desc">
-        <StyledH2>
-          Where <BlackBackground>fashion takes</BlackBackground> the helm, and you embark on a journey
-        </StyledH2>
-      </div> */}
     </HomeSection>
   );
 }
-
-
